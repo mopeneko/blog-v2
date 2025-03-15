1b6260e500b381bdf2d87c1b221ddcda1b6fed48
mopeneko <mopeneko@lem0n.cc>
2025-03-15 14:05:44 +0900

    Initialize daisyUI

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/view/tmpl/*.html", "./app/view/style.css"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
