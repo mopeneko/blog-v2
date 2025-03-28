c411823d2f969f4d3b1299038c1dd0c17b7f8f06
mopeneko <mopeneko@lem0n.cc>
2025-03-28 13:40:38 +0000

    Remove image converting

--- app/cmd/app/main.go
+++ app/cmd/app/main.go
@@ -1,17 +1,12 @@
 package main
 
 import (
-	"bytes"
 	"crypto/sha256"
 	"fmt"
 	"html/template"
 	"net/http"
-	"net/url"
 	"os"
-	"strings"
 	"time"
-
-	gohtml "golang.org/x/net/html"
 
 	"github.com/gofiber/fiber/v3"
 	"github.com/gofiber/fiber/v3/log"
@@ -74,28 +69,6 @@ func main() {
 			return c.SendStatus(http.StatusInternalServerError)
 		}
 
-		for i := range articles {
-			if articles[i].Thumbnail == nil {
-				continue
-			}
-
-			thumbSrc := articles[i].Thumbnail.Src
-			if thumbSrc == "" {
-				continue
-			}
-
-			u, err := url.Parse(thumbSrc)
-			if err != nil {
-				log.Errorw("Failed to parse thumbnail URL", "err", err)
-				continue
-			}
-
-			q := u.Query()
-			q.Add("format", "auto")
-			u.RawQuery = q.Encode()
-			articles[i].Thumbnail.Src = u.String()
-		}
-
 		return view.NewArticlesIndex(articles, cssHash).Render(c)
 	})
 
@@ -104,68 +77,6 @@ func main() {
 		if err != nil {
 			log.Errorw("Failed to fetch article", "err", err)
 			return c.SendStatus(http.StatusInternalServerError)
-		}
-
-		if article.Thumbnail != nil && article.Thumbnail.Src != "" {
-			u, err := url.Parse(article.Thumbnail.Src)
-			if err != nil {
-				log.Errorw("Failed to parse thumbnail URL", "err", err)
-				return c.SendStatus(http.StatusInternalServerError)
-			}
-
-			q := u.Query()
-			q.Add("format", "auto")
-			u.RawQuery = q.Encode()
-			article.Thumbnail.Src = u.String()
-		}
-
-		parsed, err := gohtml.Parse(strings.NewReader(article.Content))
-		if err != nil {
-			log.Errorw("Failed to parse HTML", "err", err)
-			return c.SendStatus(http.StatusInternalServerError)
-		}
-
-		var traverse func(n *gohtml.Node)
-		traverse = func(n *gohtml.Node) {
-			if n.Type == gohtml.ElementNode && n.Data == "img" {
-				for i := range n.Attr {
-					if n.Attr[i].Key == "src" {
-						u, err := url.Parse(n.Attr[i].Val)
-						if err != nil {
-							log.Errorw("Failed to parse image URL", "err", err)
-							continue
-						}
-
-						q := u.Query()
-						q.Add("format", "auto")
-						u.RawQuery = q.Encode()
-						n.Attr[i].Val = u.String()
-					}
-				}
-			}
-
-			for c := n.FirstChild; c != nil; c = c.NextSibling {
-				traverse(c)
-			}
-		}
-
-		traverse(parsed)
-
-		var buf bytes.Buffer
-		gohtml.Render(&buf, parsed)
-		article.Content = buf.String()
-
-		if article.Product != nil && article.Product.Image != nil && article.Product.Image.Src != "" {
-			u, err := url.Parse(article.Product.Image.Src)
-			if err != nil {
-				log.Errorw("Failed to parse thumbnail URL", "err", err)
-				return c.SendStatus(http.StatusInternalServerError)
-			}
-
-			q := u.Query()
-			q.Add("format", "auto")
-			u.RawQuery = q.Encode()
-			article.Product.Image.Src = u.String()
 		}
 
 		return view.NewArticle(article, cssHash).Render(c)
@@ -180,68 +91,6 @@ func main() {
 		if err != nil {
 			log.Errorw("Failed to fetch page", "err", err)
 			return c.SendStatus(http.StatusInternalServerError)
-		}
-
-		if page.Thumbnail != nil && page.Thumbnail.Src != "" {
-			u, err := url.Parse(page.Thumbnail.Src)
-			if err != nil {
-				log.Errorw("Failed to parse thumbnail URL", "err", err)
-				return c.SendStatus(http.StatusInternalServerError)
-			}
-
-			q := u.Query()
-			q.Add("format", "auto")
-			u.RawQuery = q.Encode()
-			page.Thumbnail.Src = u.String()
-		}
-
-		parsed, err := gohtml.Parse(strings.NewReader(page.Content))
-		if err != nil {
-			log.Errorw("Failed to parse HTML", "err", err)
-			return c.SendStatus(http.StatusInternalServerError)
-		}
-
-		var traverse func(n *gohtml.Node)
-		traverse = func(n *gohtml.Node) {
-			if n.Type == gohtml.ElementNode && n.Data == "img" {
-				for i := range n.Attr {
-					if n.Attr[i].Key == "src" {
-						u, err := url.Parse(n.Attr[i].Val)
-						if err != nil {
-							log.Errorw("Failed to parse image URL", "err", err)
-							continue
-						}
-
-						q := u.Query()
-						q.Add("format", "auto")
-						u.RawQuery = q.Encode()
-						n.Attr[i].Val = u.String()
-					}
-				}
-			}
-
-			for c := n.FirstChild; c != nil; c = c.NextSibling {
-				traverse(c)
-			}
-		}
-
-		traverse(parsed)
-
-		var buf bytes.Buffer
-		gohtml.Render(&buf, parsed)
-		page.Content = buf.String()
-
-		if page.Thumbnail != nil && page.Thumbnail.Src != "" {
-			u, err := url.Parse(page.Thumbnail.Src)
-			if err != nil {
-				log.Errorw("Failed to parse thumbnail URL", "err", err)
-				return c.SendStatus(http.StatusInternalServerError)
-			}
-
-			q := u.Query()
-			q.Add("format", "auto")
-			u.RawQuery = q.Encode()
-			page.Thumbnail.Src = u.String()
 		}
 
 		return view.NewPage(page, cssHash).Render(c)
